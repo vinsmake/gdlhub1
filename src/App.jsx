9decf29c64d78d9e410ac86fe97bd904407c04c6
vinsmake <ps.enrique.e@gmail.com>
2024-10-02 14:31:07 -0600

    Project updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import './css/normalize.css'
 import './css/vinsmake.css'
+import { AppRouter } from './router/AppRouter'
 
 export const App = () => {
   return (
   <>
-    <h1>GDLHUB</h1>
+    <AppRouter/>
   </>
   )
 }
---
+++ src/pages/user/RouterUser.jsx
@@ -0,0 +1,7 @@
+export const RouterUser = () => {
+    return (
+    <>
+        <div>RouterUser</div>
+    </>
+    )
+}
